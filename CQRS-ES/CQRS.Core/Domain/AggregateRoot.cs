4cd96c5a929ec7792be820e1515290b66d45f0ca
Narendra <narendra.polasamn@gmail.com>
2024-01-11 17:56:22 +0530

    dotnet kafka solution

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,60 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Domain
+{
+    public abstract class AggregateRoot
+    {
+        protected Guid _id;
+
+        protected readonly List<BaseEvent> _changes = new();
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public int Version { get; set; } = -1;
+
+        public List<BaseEvent> GetUncommitedChanges()
+        {
+            return _changes;
+        }
+
+        public void MarkChangesAsCommited()
+        {
+            _changes.Clear();
+        }
+
+        private void ApplyChange(BaseEvent @event, bool isNew) 
+        {
+            var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+            if(method is null ) {
+                throw new ArgumentNullException(nameof(method),
+                            $"The Apply method was not found in the Aggregate gor {@event.GetType().Name}");
+            }
+
+            method.Invoke( this, new object[] { @event});
+            if(isNew)
+            {
+                _changes.Add(@event);
+            }
+        }
+
+        public void RaiseEvent(BaseEvent @event)
+        {
+            ApplyChange(@event, true);
+        }
+        public void ReplyEvents(IEnumerable<BaseEvent> events)
+        {
+            foreach(var @event in events)
+            {
+                ApplyChange(@event, false);
+            }
+        }
+    }
+}
